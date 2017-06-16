b9b48401c8e76992ad2153931026f54280c166df
helderpessetti <helder@hp7.com.br>
2017-06-16 04:15:43 -0300

    Funcionando até aula 53

--- class/sql.php
+++ class/sql.php
@@ -1,33 +1,30 @@
 <?php
-
-class sql extends PDO {
+class Sql extends PDO {
     private $conn;
-
-    public function __construct () {
-        $this->conn = new PDO("mysql:dbname=dbphp7; host=localhost", "root", "h22p011958");
+    public function __construct(){
+        $this->conn = new PDO("mysql:host=localhost;dbname=dbphp7", "root", "h22p011958");
     }
-
-    public function setParams($statment, $parameters = array()){
+    private function setParams($statement, $parameters = array()){
         foreach ($parameters as $key => $value) {
-            $this->setParam($key,$value);
+
+            $this->setParam($statement, $key, $value);
         }
     }
-
-    private function setParam($statment, $key, $value){
-        $statment->bindParam($key, $value);
+    private function setParam($statement, $key, $value){
+        $statement->bindParam($key, $value);
     }
 
-    public function query($rawQuery, $params = array()) {
+    public function query($rawQuery, $params = array()){
         $stmt = $this->conn->prepare($rawQuery);
-        $this->setParams($stmt,$params);
+        $this->setParams($stmt, $params);
         $stmt->execute();
         return $stmt;
     }
 
-    public function select($rawQuery, $params = array()):array {
+    public function select($rawQuery, $params = array()):array
+    {
         $stmt = $this->query($rawQuery, $params);
         return $stmt->fetchAll(PDO::FETCH_ASSOC);
     }
 }
-
  ?>
--- index.php
+++ index.php
@@ -1,7 +1,11 @@
 <?php
     require_once("config.php");
-    $sql = new Sql();
+    /*$sql = new Sql();
     $usuarios = $sql->select("select * from tb_usuarios");
     echo json_encode($usuarios);
+    */
+    $root = new Usuario();
+    $root->loadById(2);
+    echo $root;
 
  ?>
