20b70b9756b7224dd5dfe4dae990ddc30b8180bc
helderpessetti <helder@hp7.com.br>
2017-06-16 15:24:25 -0300

    Dao INSERT

--- index.php
+++ index.php
@@ -1,11 +1,51 @@
 <?php
-    require_once("config.php");
-    /*$sql = new Sql();
-    $usuarios = $sql->select("select * from tb_usuarios");
-    echo json_encode($usuarios);
-    */
-    $root = new Usuario();
-    $root->loadById(2);
-    echo $root;
 
+require_once("config.php");
+
+/*
+//Carrega um usuário
+$root = new Usuario();
+$root->loadbyId(3);
+echo $root;
+*/
+
+/*
+//Carrega uma lista de usuários
+//$lista = Usuario::getList();
+//echo json_encode($lista);
+*/
+
+/*
+//Carrega uma lista de usuários buscando pelo login
+//$search = Usuario::search("he");
+//echo json_encode($search);
+*/
+
+/*//carrega um usuário usando o login e a senha
+$usuario = new Usuario();
+$usuario->login("helder", "helder");
+echo $usuario;*/
+
+
+//Criando um novo usuário
+$aluno = new Usuario("aluno", "@lun0");
+$aluno->insert();
+echo $aluno;
+
+/*
+//Alterar um usuário
+$usuario = new Usuario();
+
+$usuario->loadById(8);
+
+$usuario->update("professor", "!@#$%¨&*");
+
+echo $usuario;
+*/
+/* Deleta Usuario
+$usuario = new Usuario();
+$usuario->loadById(7);
+$usuario->delete();
+echo $usuario;
+*/
  ?>
